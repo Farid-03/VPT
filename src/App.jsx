9ac3bbb0bb5c7bdcb706d2bdbf4a86cdf6ae5234
Farid Ibrahimzada <ibrahimzadef26@gmail.com>
2025-02-11 15:00:10 +0400

    fp

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import Header from './components/Header'
+import { ways } from './data'
+
+function WayToTeach(props) {
+	return (
+		<li>
+			<p>
+				<strong>{props.title}</strong>
+				{props.description}
+			</p>
+		</li>
+	)
+}
+
+export default function App() {
+	return (
+		<div>
+			<Header />
+			<main>
+				<section>
+					<h3>Наш подход к обучению</h3>
+					<ul>
+						<WayToTeach
+							title={ways[0].title}
+							description={ways[0].description}
+						/>
+						<WayToTeach
+							title={ways[1].title}
+							description={ways[1].description}
+						/>
+						<WayToTeach
+							title={ways[2].title}
+							description={ways[2].description}
+						/>
+						<WayToTeach
+							title={ways[3].title}
+							description={ways[3].description}
+						/>
+					</ul>
+				</section>
+			</main>
+		</div>
+	)
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from '/public/logo-name.svg'
+export default function Header() {
+	const now = new Date()
+	return (
+		<header>
+			<img src={logo} alt={'Result'} />
+			<h3>AzETQ</h3>
+			<span>Clock : {now.toLocaleTimeString()}</span>
+		</header>
+	)
+}
