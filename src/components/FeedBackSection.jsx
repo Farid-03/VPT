9a779ca87f08436d200387fa3f3de93fc0f84710
Farid Ibrahimzada <ibrahimzadef26@gmail.com>
2025-09-12 14:44:10 +0400

    fbsection links input

--- src/components/FeedBackSection.jsx
+++ src/components/FeedBackSection.jsx
@@ -1,57 +1,86 @@
-import {useState} from "react";
-import Button from "./Button/Button";
+import { useState, useRef } from 'react'
+import Button from './Button/Button'
 
+function StateVsRef() {
+	const [value, setValue] = useState('')
+	const inputRef = useRef(null)
+
+	const handleKeyDown = e => {
+		if (e.key === 'Enter') {
+			setValue(inputRef.current.value)
+		}
+	}
+
+	console.log(inputRef)
+
+	return (
+		<div>
+			<h3>Input value: {value}</h3>
+			<input
+				type='text'
+				onKeyDown={handleKeyDown}
+				ref={inputRef}
+				className='control'
+			/>
+		</div>
+	)
+}
 export default function FeedBackSection() {
-    const [form, setForm] = useState({name: '', hasError: true, reason: 'help'})
+	const [form, setForm] = useState({
+		name: '',
+		hasError: false,
+		reason: 'help',
+	})
 
-    function handleChange(e) {
-        setForm((prev) => ({
-            ...prev,
-            name: e.target.value,
-            hasError: e
-                .target
-                .value
-                .trim()
-                .length === 0
-        }))
-    }
+	function handleChange(e) {
+		setForm(prev => ({
+			...prev,
+			name: e.target.value,
+			hasError: e.target.value.trim().length === 0,
+		}))
+	}
 
-    return (
-        <section>
-            <h3>Обратная связь</h3>
+	return (
+		<section>
+			<h3>Обратная связь</h3>
 
-            <form>
-                <label htmlFor="name">Ваше имя</label>
-                <input
-                    type="text"
-                    id="name"
-                    className="control"
-                    value={form.name}
-                    style={{
-                        border: form.hasError
-                            ? '1px solid red'
-                            : null
-                    }}
-                    onChange={handleChange}/>
+			<form style={{ marginBottom: '1rem' }}>
+				<label htmlFor='name'>Ваше имя</label>
+				<input
+					type='text'
+					id='name'
+					className='control'
+					value={form.name}
+					style={{
+						border: form.hasError ? '1px solid red' : null,
+					}}
+					onChange={handleChange}
+				/>
 
-                <label htmlFor="reason">Причина обращения</label>
-                <select
-                    id="reason"
-                    className="control"
-                    value={form.reason}
-                    onChange={(e) => setForm((prev) => ({
-                        ...prev,
-                        reason: e.target.value
-                    }))}>
-                    <option value="error">Ошибка</option>
-                    <option value="help">Нужна помощь</option>
-                    <option value="suggest">Предложение</option>
-                </select>
-                <pre>
-                    {JSON.stringify(form, null, 2)}
-                </pre>
-                <Button disabled={form.hasError} isActive={!form.hasError}>Отправить</Button>
-            </form>
-        </section>
-    )
+				<label htmlFor='reason'>Причина обращения</label>
+				<select
+					id='reason'
+					className='control'
+					value={form.reason}
+					onChange={e =>
+						setForm(prev => ({
+							...prev,
+							reason: e.target.value,
+						}))
+					}
+				>
+					<option value='error'>Ошибка</option>
+					<option value='help'>Нужна помощь</option>
+					<option value='suggest'>Предложение</option>
+				</select>
+
+				<pre>{JSON.stringify(form, null, 2)}</pre>
+
+				<Button disabled={form.hasError} isActive={!form.hasError}>
+					Отправить
+				</Button>
+			</form>
+			<StateVsRef />
+		</section>
+	)
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+import logo from '/public/logo-name.svg'
+
+export default function Header() {
+	const [time, setTime] = useState(new Date().toLocaleTimeString())
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setTime(new Date().toLocaleTimeString())
+		}, 1000)
+
+		return () => clearInterval(interval)
+	}, [])
+
+	return (
+		<header>
+			<img src={logo} alt={'Result'} />
+			<h3>AzETQ</h3>
+			<span>Clock: {time}</span>
+		</header>
+	)
+}
